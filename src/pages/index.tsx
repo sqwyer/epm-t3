8230e190eb246d253b9df665b96608b0199bf119
Sawyer Bivens <sawyer.codes.stuff@gmail.com>
2022-07-26 11:54:28 -0500

    Make navbar better, remove part of example trpc router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<div className="max-w-7xl mx-auto px-4 sm:px-6">
-        		{/* <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"></div> */}
+				{/* <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"></div> */}
 				<Nav />
 				<p>
 					{session?.user
